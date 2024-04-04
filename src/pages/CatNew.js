8da935d318c5c9764186363802ae694888f292dc
Louie-cpu <luis.u.m779@gmail.com>
2024-04-04 13:55:23 -0700

    removed form validations

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -35,11 +35,13 @@ const CatNew = ({ createNewCat }) => {
           <path d="M90,150 Q100,160 110,150" stroke="#d0d0d0" strokeWidth="5" fill="none" /> {/* Mouth */}
         </svg>
       </div>
+
+
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Col md={6}>
             <FormGroup>
-              <Label for="name">Enter Your Cat's Name</Label>
+              <Label for="name">Enter Cat's Name</Label>
               <input
                 id="name"
                 name="name"
@@ -49,7 +51,7 @@ const CatNew = ({ createNewCat }) => {
                 {...register("name", { required: true })}
               />
               {errors.name && (
-                <span className="form-validations">Name is required</span>
+                <span >Name is required</span>
               )}
             </FormGroup>
           </Col>
@@ -66,7 +68,7 @@ const CatNew = ({ createNewCat }) => {
                 {...register("age", { required: true })}
               />
               {errors.age && (
-                <span className="form-validations">Age is required</span>
+                <span >Age is required</span>
               )}
             </FormGroup>
           </Col>
@@ -82,7 +84,7 @@ const CatNew = ({ createNewCat }) => {
             {...register("enjoys", { required: true })}
           />
           {errors.enjoys && (
-            <span className="form-validations">Enjoys is required</span>
+            <span >Enjoys is required</span>
           )}
         </FormGroup>
         <FormGroup>
@@ -96,7 +98,7 @@ const CatNew = ({ createNewCat }) => {
             {...register("image", { required: true })}
           />
           {errors.image && (
-            <span className="form-validations">Image is required</span>
+            <span >Image is required</span>
           )}
         </FormGroup>
         <div className="centering-content">
